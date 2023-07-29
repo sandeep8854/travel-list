3688102afcf9964baf00d81f69081a283124e3d0
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2023-07-29 23:14:26 +0530

    adding all functionality in trvel list so user can add what ever list have

---
+++ src/components/Form.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItems = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItems);
+    onAddItems(newItems);
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 🥰 trip ?</h3>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item.."
+        value={description}
+        onChange={(e) => {
+          //  console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
